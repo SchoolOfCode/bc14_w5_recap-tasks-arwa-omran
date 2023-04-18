e16f2595f085501a1c1d44708833fc97d736f143
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-19 01:21:13 +0300

    tests passed

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -5,8 +5,10 @@ const BlogPost = ({ blog }) => {
   return (
     <div className="blog-post">
       <img src={blog.imageSrc} alt={blog.imageAlt} />
+      <p className="caption">{blog.imageAlt}</p>
       <h2>{blog.title}</h2>
-      <p className="author">{`By ${blog.author} on ${blog.datePosted}`}</p>
+      <h3 className="author">{`By ${blog.author}`}</h3>
+      <p className="date">{blog.datePosted}</p>
       <p className="content">{blog.content}</p>
     </div>
   );
--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,43 +1,12 @@
-// // CommentForm.js
-// import React, { useState } from "react";
-
-// function CommentForm({ onSubmit }) {
-//   const [author, setAuthor] = useState("");
-//   const [content, setContent] = useState("");
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     if (content.trim() !== "") {
-//       onSubmit({ author, content });
-//       setContent("");
-//     }
-//   };
-
-//   return (
-//     <form >
-//       <div>
-//         <label>Author</label>
-//         <input type="text" id="author" value={author} onChange={(event) => setAuthor(event.target.value)} />
-//       </div>
-//       <div>
-//         <label>Comment</label>
-//         <textarea id="content" value={content} onChange={(event) => setContent(event.target.value)} />
-//       </div>
-//       <button type="submit" onSubmit={handleSubmit}>Submit</button>
-//     </form>
-//   );
-// }
-
-// export default CommentForm;
-
 import React, { useState } from "react";
+
 function CommentForm({ onSubmit }) {
-  const [author, setAuthor] = useState("");
+  const [author, setAuthor] = useState("Anon");
   const [content, setContent] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent page refresh so data is not lost
-    if (content.trim() !== "") { // check if content is not empty
+    if (content.trim() !== "" && author.trim() !== "") { // check if content and author are not empty
       onSubmit(author, content); // pass the author and content as separate arguments
       setAuthor(""); // reset author and content after submitting
       setContent("");
@@ -45,16 +14,16 @@ function CommentForm({ onSubmit }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}> {/* attach onSubmit listener to form element */}
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Author</label>
+        <label htmlFor="author">Author</label>
         <input type="text" id="author" value={author} onChange={(event) => setAuthor(event.target.value)} />
       </div>
       <div>
-        <label>Comment</label>
+        <label htmlFor="content">Comment</label>
         <textarea id="content" value={content} onChange={(event) => setContent(event.target.value)} />
       </div>
-      <button type="submit">Submit</button> {/* dont use onClick here because it will not work with the form element*/}
+      <button type="submit">Submit</button>
     </form>
   );
 }
